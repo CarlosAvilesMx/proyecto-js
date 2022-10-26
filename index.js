0baf3a72b5b54b374642b1b17250693b15834435
Carlos Aviles <aviles.martinez@gmail.com>
2022-10-26 07:50:31 -0500

    Ultimo

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const fn = () => {
     const miString = "soy una string!"
     let b = miString + " muy feliz"
     console.log(b)
+    console.log('hola papucho');
 }
 
 fn()
 
 const clickeado = () => {
     let a = "chanchito feliz"
-    console.log(a)
+    /console.log(a)
 }
